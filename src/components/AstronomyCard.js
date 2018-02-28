1bcc442b2ac75ee923b501843e5e5da62f394d1b
laurenperez <laurenashleymarks@gmail.com>
2018-02-28 12:44:37 -0800

    init

---
+++ src/components/AstronomyCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../index.css';
+
+
+const AstronomyCard = (props) => {
+
+  const { title, url, hdurl, explanation, date, copyright } = props.data
+
+  return (
+    <div className="astronomy-card">
+      <h6 className="astronomy-title">{title}</h6>
+      <a href={hdurl} className="astronomy-image-wrapper">
+        <img src ={url} alt={title} />
+      </a>
+      <p>{explanation}</p>
+      <span>{date}</span>
+    </div>
+  )
+}
+
+
+export default AstronomyCard;
---
+++ src/components/AstronomyContainer.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import AstronomyCard from './AstronomyCard';
+import axios from 'axios';
+
+class AstronomyContainer extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      astronomy: []
+    }
+  }
+
+
+  componentDidMount() {
+    const API_KEY = 'VRwTIuCIT0mKviiPsMeTUxFlLLt1DgAndhDsVHlL';
+    const END_POINT = 'https://api.nasa.gov/planetary/apod?api_key=';
+
+    axios.get(END_POINT+API_KEY)
+    .then(response => {
+
+      this.setState({
+        astronomy: response.data
+      })
+
+      console.log(this.state.astronomy)
+
+    })
+    .catch(error => {
+      console.log(error, 'failed to catch data')
+    });
+  }
+
+
+  render () {
+    const {astronomy} = this.state;
+    return (
+      <AstronomyCard data={astronomy} />
+    )
+  }
+
+}
+
+export default AstronomyContainer;
